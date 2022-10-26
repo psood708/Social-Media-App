5a054521bd4ea3b9d8b7f24ed754a4ba5cbb71e8
Parth <parthsood99@gmail.com>
2022-10-26 15:12:59 +0530

    Sanity for backend and react-tailwind for frontend

---
+++ proj1-frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App  =() =>{
+
+    <div>
+        <div>
+            <h1>Title</h1>
+            <p>This is social media app</p>
+        </div>
+    </div>
+}
+export default App 
